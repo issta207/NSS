6ca951c9fa9b84f9aa03ae8c06d20ca43ed8c5fd
issta207 <125374999+issta207@users.noreply.github.com>
2023-02-17 16:24:39 +0800

    Update FDR&FDD_eval.py

--- FDR&FDD_eval.py
+++ FDR&FDD_eval.py
@@ -11,12 +11,9 @@ from torchvision.transforms import ToTensor
 # from feature_wise_pgd import PGD
 import argparse
 import random
-from PGD import PGD
-# from mcmc import MCMC
 from resnet20 import ResNet20
 from LeNet import LeNet5
 from benign_perturbations import benign_aug
-from deepgini import deep_metric, intersection
 
 import torchattacks
 
